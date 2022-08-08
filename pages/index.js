56c79683296a6a7766a08496ffb5d266ca765f79
jazzDanielse <danielcuric@gmail.com>
2022-08-08 11:08:30 +0200

    kresko

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Naslovna() {
   return (
     <div className="h-screen w-screen bg-gradient-to-r from-cyan-500 to-blue-500">
       <h6 className="uppercase text-center text-5xl antialiased font-bold text-pink-900 p-5">
-        Dobrodošli u Krešo fan page
+        Dobrodošli u Kreško fan page
       </h6>
     </div>
   );
--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,7 +1,7 @@
 export default function Kontakt() {
   return (
     <div className="w-screen h-screen bg-gradient-to-r from-cyan-w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500 to-blue-500">
-      <h6 className="uppercase text-center text-5xl antialiased font-bold text-pink-900 p-5">
+      <h6 className="opacity-60	max-w-full uppercase text-center text-5xl antialiased font-bold text-pink-900 p-5">
         Kontaktirajte nas ako ste i vi prosijede pederčine
       </h6>
     </div>
