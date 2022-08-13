aeb3f8e8cf08eceb4b6b65d2e916917cdb7eaed2
jazzDanielse <danielcuric@gmail.com>
2022-08-13 09:02:00 +0200

    promjena naslova opet

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -2,7 +2,7 @@ export default function Kontakt() {
   return (
     <div className="w-screen h-screen bg-hero bg-no-repeat bg-center bg-scroll bg-auto">
       <div className="bg-slate-200">
-        <h6 className="uppercase text-center antialiased font-bold text-pink-900 p-5 text-center text-xs">
+        <h6 className="uppercase text-center antialiased font-bold text-pink-900 p-5 text-center text-2xl">
           Kontaktirajte nas ako ste i vi prosijede pederčine
         </h6>
       </div>
